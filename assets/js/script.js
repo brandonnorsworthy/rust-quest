40f6fc89c304f5674228d01ac563b7021c1767a7
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-18 05:14:29 -0500

    added events onto buttons to show card and call a mission, removed old obj file, slight css adjustments to get controls above wheel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 
-function init() {
 
+//! HTML Screens
+let missionCardEl = document.getElementById("mission-card") //card that displays missions
+let paintWheelEl = document.getElementById("paint-stroke") //paint stroke mask that holds the wheel inside
+
+function createEventListeners() {
+    //todo sound design for completions
+    document.getElementById("spin-wheel").addEventListener("click", () => {
+        missionCardEl.style.display = "block";
+        buildHtml(getMission(), "PVP")
+    })
+
+    document.getElementById("complete").addEventListener("click", () => {
+        missionCardEl.style.display = "none";
+    })
+
+    document.getElementById("skip").addEventListener("click", () => {
+        missionCardEl.style.display = "none";
+    })
 }
 
-init()
+createEventListeners();
