e7b0ebc116849dfd41eb654b224387d96fb1b220
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-18 01:00:12 -0500

    remove querySelectors that were left over from old html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 
 function init() {
-    document.querySelector("button").addEventListener("click", buttonClick)
-}
 
-function buttonClick() {
-    document.querySelector("p").textContent = 'testawwefwf'
 }
 
 init()
