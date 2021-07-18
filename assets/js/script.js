e89808b59ecd00add3685bd2f6834d96506998b2
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-17 19:29:44 -0500

    basic buildout with functional buttons ready for js expansion

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+function init() {
+    document.querySelector("button").addEventListener("click", buttonClick)
+}
+
+function buttonClick() {
+    document.querySelector("p").textContent = 'testawwefwf'
+}
+
+init()
