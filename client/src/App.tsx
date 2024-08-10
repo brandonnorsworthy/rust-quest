cdc07a80b4a98c13d9b68552b18f24ec9270621a
Jered Spears <jered.spears@zealitconsultants.com>
2024-08-10 12:29:35 -0500

    created base vite app with tailwind, move client -> clien_dep

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header className='text-red-400'>
+        <h3>
+          Welcome to the resurected rust app
+        </h3>
+      </header>
+      <main>
+        <h1>Counter</h1>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </main>
+    </div>
+  )
+}
+
+export default App
